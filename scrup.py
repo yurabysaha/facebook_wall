16aad56697fb73517bb2b02efb396acfaf784187
thinkmobilesqa <oleg.stasiv@thinkmobiles.com>
2017-05-20 20:38:22 +0300

    add scrup file.

---
+++ scrup.py
@@ -0,0 +1,70 @@
+import facebook
+import requests
+
+
+class Scrup:
+
+    def __init__(self):
+        pass
+
+    def _is_number(self, s):
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+    def getAllPostsFromGroup(self, token, group_url):
+        self.graph = facebook.GraphAPI(access_token=token, version='2.3')
+        # Get ID from group Url
+        get_group_name = group_url.rstrip('/ ').split('/')[-1]
+        if self._is_number(get_group_name) == True:
+            group_id = get_group_name
+        else:
+            resposne = self.graph.search(type='group', q=get_group_name)
+            group_id = resposne['data'][0]['id']
+            group_name = resposne['data'][0]['name']
+        # ---------------------
+        # Get feeds from Group
+        data = self.graph.get_connections(group_id, 'feed')
+        count_posts = 1
+        while (data['data']):
+            try:
+                for post in data['data']:
+                    print ("-------------"+str(count_posts)+"------------------")
+                    try:
+                        print ('Post was created: ' + post['created_time'])
+                    except:
+                        pass
+                    try:
+                        print ('Who create post: ' + post['from']['name'] + '. ID: ' + post['from']['id'])
+                    except:
+                        pass
+                    try:
+                        print ('Description: ' + post['description'])
+                    except:
+                        pass
+                    try:
+                        print ('Message: ' + post['message'])
+                    except:
+                        pass
+                    try:
+                        print ('Picture link:  ' + post['picture'])
+                    except:
+                        pass
+                    try:
+                        print ('Link:  ' + post['link'])
+                    except:
+                        pass
+                    try:
+                        print ('Source: :  ' + post['source'])
+                    except:
+                        pass
+                    count_posts += 1
+                # Loading to other page
+                data = requests.get(data['paging']['next']).json()
+                # ---------------------
+            except KeyError:
+                print "Key Error"
+
+
